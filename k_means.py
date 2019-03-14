a0560c9bf46587f66365d8c2377dc7161fdd001a
rektrex <psaimanideep98@gmail.com>
2019-03-14 06:39:29 +0530

    reduced to 18 sloc

--- k_means.py
+++ k_means.py
@@ -1,30 +1,22 @@
 from random import sample
 from copy import deepcopy
 
-def dist(x, y):
-    return sum(list(map(lambda t: (t[0] - t[1]) ** 2, list(zip(x, y))))) ** (0.5)
-
-def mean(lot):
-    return tuple([sum(t) / len(t) for t in zip(*lot)])
-
 def k_means(points, k):
     centres = [points[i] for i in sample(list(range(len(points))), k)]
     oldclusters = [[] for _ in range(k)]
     while True:
         clusters = [[] for _ in range(k)]
         for p in points:
-            dists = list(map(lambda x: dist(x, p), centres))
-            c = dists.index(min(dists))
-            clusters[c].append(p)
+            dists = list(map(lambda x: sum(list(map(lambda t: (t[0] - t[1]) ** 2, list(zip(x, p))))) ** (0.5), centres))
+            clusters[dists.index(min(dists))].append(p)
 
         if clusters == oldclusters:
             return clusters
 
-        centres = list(map(mean, clusters))
+        centres = list(map(lambda c: tuple([sum(t) / len(t) for t in zip(*c)]), clusters))
         oldclusters = deepcopy(clusters)
 
 if __name__ == '__main__':
     points = [(1.0, 1.0), (1.5, 2.0), (3.0, 4.0), (5.0, 7.0), (3.5, 5.0), (4.5, 5.0), (3.5, 4.5)]
-    k = 3
-    result = k_means(points, k)
+    result = k_means(points, k = 3)
     print(*result, sep = '\n')
